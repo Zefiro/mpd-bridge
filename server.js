00f84b352eb53a3c5ed38a12f4dae6c899241c81
Zefiro <github.com@spam.zefiro.de>
2018-10-31 01:19:40 +0100

    updated dependencies

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const {promisify} = require('util')
 const fetch = require('node-fetch');
 var SqueezeServer = require('squeezenode');
 var squeeze = new SqueezeServer('http://localhost', 9000);
+const dict = require("dict")
 
 /* see https://unix.stackexchange.com/questions/81754/how-can-i-match-a-ttyusbx-device-to-a-usb-serial-device
    # lsusb && ll /sys/bus/usb-serial/devices && ls -l /dev/serial/by-id
@@ -89,6 +90,14 @@ http.listen(8080, function(){
   console.log('listening on *:8080')
 })
 
+// Test for Eslar / Slushmachine
+// "[1-56 effect] [0-255 red] [0-255 green] [0-255 blue] [z.B. 1000 speed]"
+// https://github.com/kitesurfer1404/WS2812FX
+web.addListener("client", "0",         async (req, res) => "2 0 255 0 1000")
+web.addListener("client", "1",         async (req, res) => "2 64 128 192 1000")
+web.addListener("client", "2",         async (req, res) => "2 0 255 0 1000")
+web.addListener("client", "3",         async (req, res) => "2 250 100 20 1000")
+
 
 web.addListener("mpd", "fadePause",       async (req, res) => fadePause(1))
 web.addListener("mpd", "fadePlay",        async (req, res) => fadePlay(1))
@@ -104,6 +113,8 @@ web.addListener("redButton", "ping", async (req, res) => "pong")
 
 web.addListener("cave", "speakerOn",         async (req, res) => extender2('Speaker', 'on'))
 web.addListener("cave", "speakerOff",        async (req, res) => extender2('Speaker', 'off'))
+web.addListener("cave", "LightOn",         async (req, res) => { openhab('light_sofa', 'ON'); openhab('light_pc', 'ON') })
+web.addListener("cave", "LightOff",         async (req, res) => { openhab('light_sofa', 'OFF'); openhab('light_pc', 'OFF') })
 
 wodoinco.addListener("A Tast A",  async (txt) => { console.log("WoDoInCo: Light toggled: " + txt) })
 wodoinco.addListener("A Tast B",  async (txt) => { extender2('Speaker', 'on'); console.log(await fadePlay(2)) })
@@ -111,16 +122,42 @@ wodoinco.addListener("A Tast C",  async (txt) => { extender2('Speaker', 'timed-o
 wodoinco.addListener("A Tast Do", async (txt) => { console.log(await changeVolume(+2)) })
 wodoinco.addListener("A Tast Du", async (txt) => { console.log(await changeVolume(-2)) })
 
-extender.addListener(0 /* green           */, 1, async (pressed, butValues) => { console.log(await fadePlay(2)) })
+extender.addListener(0 /* green           */, 1, async (pressed, butValues) => { console.log(await fadePlay(2)); })
 extender.addListener(1 /* red             */, 1, async (pressed, butValues) => { console.log(await fadePause(0)) })
 extender.addListener(2 /* tiny blue       */, 1, async (pressed, butValues) => { openhab('alarm', 'TOGGLE') })
 extender.addListener(3 /* tiny red        */, 1, async (pressed, butValues) => { regalbrett('alarm') })
-extender.addListener(4 /* tiny yellow     */, 1, async (pressed, butValues) => { regalbrett('disco') })
+extender.addListener(4 /* tiny yellow     */, 1, async (pressed, butValues) => { regalbrett('disco'); openhab('light_sofa', 'OFF'); openhab('light_pc', 'OFF') })
 extender.addListener(5 /* tiny green      */, 1, async (pressed, butValues) => { regalbrett('calm'); openhab('alarm', 'OFF') })
-extender.addListener(6 /* red switch      */, 1, async (pressed, butValues) => { extender2('Speaker', 'on'); wodoinco2('Light', 'on') })
-extender.addListener(6 /* red switch      */, 0, async (pressed, butValues) => { extender2('Speaker', 'off'); wodoinco2('Light', 'off') })
-extender.addListener(7 /* big blue switch */, 1, async (pressed, butValues) => { openhab('FensterLedNetz', 'ON'); openhab('Monitors', 'ON'); openhab('Regalbrett', 'ON') })
-extender.addListener(7 /* big blue switch */, 0, async (pressed, butValues) => { openhab('FensterLedNetz', 'OFF'); openhab('Monitors', 'OFF'); openhab('Regalbrett', 'OFF') })
+extender.addListener(6 /* red switch (on) */, 1, async (pressed, butValues) => { extender2('Speaker', 'on'); wodoinco2('Light', 'on') })
+extender.addListener(6 /* red switch (off)*/, 0, async (pressed, butValues) => { extender2('Speaker', 'off'); wodoinco2('Light', 'off') })
+extender.addListener(7 /* big blue switch */, 1, async (pressed, butValues) => { openhab('FensterLedNetz', 'ON'); openhab('Monitors', 'ON'); openhab('Regalbrett', 'ON'); openhab('Regalbrett2', 'ON') })
+extender.addListener(7 /* big blue switch */, 0, async (pressed, butValues) => { openhab('FensterLedNetz', 'OFF'); openhab('Monitors', 'OFF'); openhab('Regalbrett', 'OFF'); openhab('Regalbrett2', 'OFF') })
+
+// TODO WIP
+var waschmaschine = {}
+
+timer = {
+	timers: dict(),
+	watchChange: async function(name, intervalSec, fnWatch, fnOnChange) {
+		var self = this
+		console.log("Timer: added change watch '%s' every %d sec", name, intervalSec)
+		var timerId = setInterval(async () => {
+			let value = await fnWatch()
+			let lastValue = self.timers.get(name).lastValue
+			self.timers.get(name).lastValue = value
+			if (value != lastValue) {
+				console.log("Timer: change detected for '%s': %s -> %s", name, lastValue, value)
+				await fnOnChange(value)
+			}
+		}, intervalSec * 1000)
+		self.timers.set(name, { id: timerId })
+		return timerId
+	}
+}
+
+
+timer.watchChange("WaMa_On", 60, () => openhabQuery('waschmaschine', 'state'), (state) => { if (state == 'ON') { waschmaschine.onSince = new Date(); regalbrett('blue_fire') } else { waschmaschine.onSince = null }})
+timer.watchChange("WaMa_Finished", 60, () => waschmaschine.onSince && (new Date() - waschmaschine.onSince > 90 * 60 * 1000), (value) => { if (value) { regalbrett('green_fire'); console.log(waschmaschine.onSince); waschmaschine.onSince += 5 * 60 * 1000; console.log(waschmaschine.onSince) } })
 
 
 console.log('Press <ctrl>+C to exit.')
@@ -138,26 +175,51 @@ async function regalbrett(scenarioName) {
 	}
 }
 
+var openhabMapping = dict({
+	"light_sofa": 'zwave_device_controller_node5_switch_binary2',
+	"light_pc": 'zwave_device_controller_node5_switch_binary',
+	"light_wc": 'zwave_device_controller_node10_switch_binary',
+	"alarm": 'Alarm',
+	"FensterLedNetz": 'FensterLednetz_Switch',
+    "Regalbrett": 'SwitchRegalbrett_Switch',
+    "Regalbrett2": 'ZWaveNode4_Switch',
+    "Monitors": 'PC_Monitors',
+	'waschmaschine': 'Waschmaschine_Switch',
+})
+
 async function openhab(item, action) {
-	let itemId = ""
-	if (item == "light_sofa") itemId = 'zwave_device_controller_node5_switch_binary2'
-	if (item == "light_pc") itemId = 'zwave_device_controller_node5_switch_binary'
-	if (item == "light_wc") itemId = 'zwave_device_controller_node10_switch_binary'
-	if (item == "alarm") itemId = 'Alarm'
-	if (item == "FensterLedNetz") itemId = 'FensterLednetz_Switch'
-    if (item == "Regalbrett") itemId = 'SwitchRegalbrett_Switch'
-    if (item == "Monitors") itemId = 'PC_Monitors'
+	let itemId = openhabMapping.get(item)
 	if (!itemId) {
 		console.log("OpenHAB: item '" + item + "' unknown")
+		return
 	}
 	console.log("OpenHAB: sending '" + action + "' to item " + item + " (" + itemId + ")")
 	try {
-		let res = await fetch('http://localhost/rest/items/' + itemId, { method: "POST", headers: [ 'Content-Type: text/plain', 'Accept: application/json' ], body: action })
+		let res = await fetch('http://localhost/rest/items/' + itemId, { method: "POST", headers: { 'Content-Type': 'text/plain', 'Accept': 'application/json' }, body: action })
 		let resText = await res.text()
 		console.log("OpenHAB responsed (" + res.status + " " + res.statusText + "): " + resText)
 	} catch(e) {
-		console.log("OpenHAB Error: ")
-		console.log(e)
+		console.log("OpenHAB Error:", e)
+	}
+}
+
+/* Return the current status of an item in openhab, or (optionally) the value of a specific field in it
+ */
+async function openhabQuery(item, key) {
+	let itemId = openhabMapping.get(item)
+	if (!itemId) {
+		console.log("OpenHAB: item '" + item + "' unknown")
+		return
+	}
+//	console.log("OpenHAB: querying status of item " + item + " (" + itemId + ")")
+	try {
+		let res = await fetch('http://localhost/rest/items/' + itemId, { method: "GET", headers: { 'Content-Type': 'text/plain', 'Accept': 'application/json' } })
+		let resJson = await res.json()
+		let data = key ? resJson[key] : resJson
+//		console.log("OpenHAB response for item '%s':", item, data)
+		return data
+	} catch(e) {
+		console.log("OpenHAB Error:", e)
 	}
 }
 
