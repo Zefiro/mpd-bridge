740fb99f17f00154e5540ce968570d0b8d0d79ee
Zefiro <github.com@spam.zefiro.de>
2020-03-28 00:01:51 +0100

    repaired merge

--- grag.js
+++ grag.js
@@ -139,16 +139,6 @@ timer = {
 	}
 }
 
-let doLaterFunc = undefined
-async function doLater(func, seconds) {
-	clearTimeout(doLaterFunc)
-	timerSpeaker = setTimeout(async function() {
-		return await func()
-	}, seconds * 1000)
-	return "Do something " + seconds + " seconds later"
-}
-
-
 /* Call functions on repeated button presses
  * 
  * Returns an async function which counts invocations and calls the given callback when 'count' invocations have occured in the last 'sec' seconds
